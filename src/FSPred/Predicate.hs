903af6c4036516ddff266057f779089985bf224c
Takumi Crary <tcrary@gmail.com>
2021-06-14 06:19:46 +0100

    Inital commit of FSPattern combinator idea

---
+++ src/FSPred/Predicate.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module FSPred.Predicate where
+
+import System.FilePath()
+import Data.Semigroup()
+import Control.Applicative
+
+type Serial = String
+
+data FSPattern =
+    DirectoryExists FilePath [FSPattern]
+  | FileSetExists [FSPattern]
+  | FileExists FilePath
+  deriving Show
+
+--Test fixtures
+root :: [FSPattern] -> FilePath -> FSPattern
+root xs = flip DirectoryExists xs
+
+genSuffixFSet :: [FilePath] -> FilePath -> FSPattern
+genSuffixFSet xs s = FileSetExists $ FileExists <$> liftA2 (++) [s] xs
+
+genTs :: Serial -> FilePath -> FSPattern
+genTs s = root [
+            DirectoryExists ("qc"<>s) [
+                   genSuffixFSet [".fbd",".stl",".fit.stl"] s,
+                   FileExists ("qc_"<>s<>".3dm"),
+                   FileExists "qc_report.txt",
+                   FileExists "Alignment.ginspect"
+               ],
+            FileExists "Calib.ini",
+            FileExists "Foot.ini",
+            FileExists "Layer.cld",
+            FileExists "Model.ini",
+            FileExists "table.ctd"
+          ]
+{-
+genTs :: FilePath -> FSPattern
+genTs s = DirectoryExists s [pattern]
+    where pattern =
+            DirectoryExists "qc"<>s (genSerialSet s : [
+                   FileExists "qc_report.txt",
+                   FileExists "Alignment.ginspect"
+               ]
+            )
+
+-}
+--How do we support annotations for file validation/verification?
+--What if we used an infix '/' operator for the directory type constructor???
+
+
+--What about parsing something like this
+{-
+ - foo/
+ -      baz.txt
+ - bar/
+ -      quux.txt
+ -}
+--Into a pattern
